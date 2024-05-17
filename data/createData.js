e764baf5c105bdefa9d5cae8a84d1ac719edd58c
WilliamCommandeur <william.commandeur@gmail.com>
2024-05-17 15:23:05 +0200

    add log in .gitignore

--- data/createData.js
+++ data/createData.js
@@ -1,79 +1,81 @@
 const { faker } = require('@faker-js/faker/locale/fr');
 const { generateRandomInt } = require('../app/util/randomInt');
-const {User, Vehicle, Address, Review} = require('../app/models');
+const {
+  User, Vehicle, Address, Review,
+} = require('../app/models');
 
 /**
  * Creates a new user with random data
  * @returns {User} A new instance of User class
  */
-function createUser(){
-    const firstname = faker.person.firstName();
-    const lastname = faker.person.lastName();
-    const username = `${lastname.toLowerCase()}_${firstname.toLowerCase()}${faker.number.int(99)}`;
-    const email = faker.internet.email({firstName: firstname, lastName:lastname});
-    const password = faker.internet.password();
-    const nationality = faker.location.country();
-    const birthdate = `${faker.date.birthdate()}`;
+function createUser() {
+  const firstname = faker.person.firstName();
+  const lastname = faker.person.lastName();
+  const username = `${lastname.toLowerCase()}_${firstname.toLowerCase()}${faker.number.int(99)}`;
+  const email = faker.internet.email({ firstName: firstname, lastName: lastname });
+  const password = faker.internet.password();
+  const nationality = faker.location.country();
+  const birthdate = `${faker.date.birthdate()}`;
 
-    const user = new User(firstname, lastname, username, email, password, nationality, birthdate);
-    return user;
+  const user = new User(firstname, lastname, username, email, password, nationality, birthdate);
+  return user;
 }
 
 /**
  * Create a new vehicle with random data
  * @returns {Vehicle} A new instance of Vehicle class
  */
-function createVehicle(){
-    const vehicleType = ['essence', 'diesel', 'électrique'];
-    const type = vehicleType[generateRandomInt(0,2)];
-    const nbPlaces = generateRandomInt(2, 7);
-    const vehiclePlate = faker.vehicle.vrm();
-    const modele = faker.vehicle.vehicle();
-    const price = faker.number.float({fractionDigits:1, min:10, max:200});
-    const ownerId = generateRandomInt(1,1000);
+function createVehicle() {
+  const vehicleType = ['essence', 'diesel', 'électrique'];
+  const type = vehicleType[generateRandomInt(0, 2)];
+  const nbPlaces = generateRandomInt(2, 7);
+  const vehiclePlate = faker.vehicle.vrm();
+  const modele = faker.vehicle.vehicle();
+  const price = faker.number.float({ fractionDigits: 1, min: 10, max: 200 });
+  const ownerId = generateRandomInt(1, 1000);
 
-    const vehicle = new Vehicle(type, nbPlaces, vehiclePlate, modele, price, ownerId);
-    return vehicle;
+  const vehicle = new Vehicle(type, nbPlaces, vehiclePlate, modele, price, ownerId);
+  return vehicle;
 }
 
 /**
  * Creates a new address with random data
  * I need to pass a parameter variable that will be incremented when I import my data into the database so that each user has an associated address.
- * @param {number} counter - The ID of the owner associated with the address 
+ * @param {number} counter - The ID of the owner associated with the address
  * @returns {Address} A new instance of Address class
  */
-function createAddress(counter){
-    const city = faker.location.city();
-    const country = 'France';
-    const street = faker.location.streetAddress();
-    const zipcode = faker.location.zipCode();
-    const additionnal = faker.location.secondaryAddress();
-    const ownerId = counter;
+function createAddress(counter) {
+  const city = faker.location.city();
+  const country = 'France';
+  const street = faker.location.streetAddress();
+  const zipcode = faker.location.zipCode();
+  const additionnal = faker.location.secondaryAddress();
+  const ownerId = counter;
 
-    const address = new Address(city, country, street, zipcode, additionnal, ownerId);
-    return address;
+  const address = new Address(city, country, street, zipcode, additionnal, ownerId);
+  return address;
 }
 
 /**
  * Creates a new review with random data
  * @returns {Review} A new instance of Review class
  */
-function createReview(){
-    const authorId = generateRandomInt(1, 1000);
-    let targetId = generateRandomInt(1, 1000);
-    if (targetId === authorId) {
-        targetId = generateRandomInt(1, 1000);
-    };
-    const rating = generateRandomInt(100, 500) / 100;
-    const comment = faker.lorem.words(15);
+function createReview() {
+  const authorId = generateRandomInt(1, 1000);
+  let targetId = generateRandomInt(1, 1000);
+  if (targetId === authorId) {
+    targetId = generateRandomInt(1, 1000);
+  }
+  const rating = generateRandomInt(100, 500) / 100;
+  const comment = faker.lorem.words(15);
 
-    const review = new Review(authorId, targetId, rating, comment);
-    return review;
+  const review = new Review(authorId, targetId, rating, comment);
+  return review;
 }
 
 module.exports = {
-    createUser,
-    createVehicle,
-    createAddress,
-    createReview
+  createUser,
+  createVehicle,
+  createAddress,
+  createReview,
 };
--- data/importData.js
+++ data/importData.js
@@ -1,81 +1,80 @@
 require('dotenv').config();
-const client = require('../app/database/client'); 
-const { createUser, createVehicle, createAddress, createReview} = require('./createData');
-
-
+const client = require('../app/database/client');
+const {
+  createUser, createVehicle, createAddress, createReview,
+} = require('./createData');
 
 /**
  * Import users into the database
- * @param {number} quantity - number of rows to import 
+ * @param {number} quantity - number of rows to import
  * @returns {Promise<Array>} - A promise that resolves to an array of database queries
  */
-function importUsers(quantity){
-    const inserts = [];
-    for (let userIndex = 0; userIndex < quantity; userIndex++){
-        const user = createUser();
-        const query = {
-            text: 'INSERT INTO "user"(firstname, lastname, username, email, password, nationality, birthdate) VALUES ($1, $2, $3, $4, $5, $6, $7);',
-            values: [user.firstname, user.lastname, user.username, user.email, user.password, user.nationality, user.birthdate]
-        };
-        inserts.push(client.query(query));
+function importUsers(quantity) {
+  const inserts = [];
+  for (let userIndex = 0; userIndex < quantity; userIndex++) {
+    const user = createUser();
+    const query = {
+      text: 'INSERT INTO "user"(firstname, lastname, username, email, password, nationality, birthdate) VALUES ($1, $2, $3, $4, $5, $6, $7);',
+      values: [user.firstname, user.lastname, user.username, user.email, user.password, user.nationality, user.birthdate],
     };
-    return Promise.all(inserts);
+    inserts.push(client.query(query));
+  }
+  return Promise.all(inserts);
 }
 
 /**
  * Import vehicles into the database
  */
-function importVehicles(quantity){
-    const inserts = [];
-    for (let vehicleIndex = 0; vehicleIndex < quantity; vehicleIndex++){
-        const vehicle = createVehicle();
-        const query = {
-            text: 'INSERT INTO vehicle(type, nb_places, vehicle_plate, modele, price, owner_id) VALUES ($1, $2, $3, $4, $5, $6);',
-            values: [vehicle.type, vehicle.nbPlaces, vehicle.vehiclePlate, vehicle.modele, vehicle.price, vehicle.ownerId]
-        }
-        inserts.push(client.query(query));
+function importVehicles(quantity) {
+  const inserts = [];
+  for (let vehicleIndex = 0; vehicleIndex < quantity; vehicleIndex++) {
+    const vehicle = createVehicle();
+    const query = {
+      text: 'INSERT INTO vehicle(type, nb_places, vehicle_plate, modele, price, owner_id) VALUES ($1, $2, $3, $4, $5, $6);',
+      values: [vehicle.type, vehicle.nbPlaces, vehicle.vehiclePlate, vehicle.modele, vehicle.price, vehicle.ownerId],
     };
-    return Promise.all(inserts)
+    inserts.push(client.query(query));
+  }
+  return Promise.all(inserts);
 }
 
 /**
  * Import addresses into the database
  */
-function importAddresses(quantity){
-    const inserts = [];
-    for (let addressIndex = 0; addressIndex < quantity; addressIndex++){
-        const address = createAddress(addressIndex+1);
-        const query = {
-            text: 'INSERT INTO address(city, country, street, zipcode, additionnal, owner_id) VALUES ($1, $2, $3, $4, $5, $6);',
-            values: [address.city, address.country, address.street, address.zipcode, address.additionnal, address.ownerId]
-        };
-        inserts.push(client.query(query));
+function importAddresses(quantity) {
+  const inserts = [];
+  for (let addressIndex = 0; addressIndex < quantity; addressIndex++) {
+    const address = createAddress(addressIndex + 1);
+    const query = {
+      text: 'INSERT INTO address(city, country, street, zipcode, additionnal, owner_id) VALUES ($1, $2, $3, $4, $5, $6);',
+      values: [address.city, address.country, address.street, address.zipcode, address.additionnal, address.ownerId],
     };
-    return Promise.all(inserts)
+    inserts.push(client.query(query));
+  }
+  return Promise.all(inserts);
 }
 
 /**
  * Import reviews into the database
  */
-function importReviews(quantity){
-    const inserts = [];
-    for (let reviewIndex = 0; reviewIndex < quantity; reviewIndex++){
-        const review = createReview();
-        const query = {
-            text: 'INSERT INTO review(author_id, target_id, rating, comment) VALUES ($1, $2, $3, $4);',
-            values: [review.authorId, review.targetId, review.rating, review.comment]
-        };
-        inserts.push(client.query(query));
-    }
-    return Promise.all(inserts);
+function importReviews(quantity) {
+  const inserts = [];
+  for (let reviewIndex = 0; reviewIndex < quantity; reviewIndex++) {
+    const review = createReview();
+    const query = {
+      text: 'INSERT INTO review(author_id, target_id, rating, comment) VALUES ($1, $2, $3, $4);',
+      values: [review.authorId, review.targetId, review.rating, review.comment],
+    };
+    inserts.push(client.query(query));
+  }
+  return Promise.all(inserts);
 }
 
-
-client.connect()
-    .then(() => console.log("Import des données..."))
-    .then(() => importUsers(1000))
-    .then(() => importVehicles(1250))
-    .then(() => importAddresses(1000))
-    .then(() => importReviews(4500))
-    .then(() => console.log("Import des données terminé"))
-    .finally(() => client.end());
+Promise.resolve()
+  .then(() => console.log('Import des données...'))
+  .then(() => importUsers(1000))
+  .then(() => importVehicles(1250))
+  .then(() => importAddresses(1000))
+  .then(() => importReviews(4500))
+  .then(() => console.log('Import des données terminé'))
+  .finally(() => client.end());
